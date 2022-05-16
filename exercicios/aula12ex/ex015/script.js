475da0de598ebe531af5db325026b7cabf2dee42
TrevoYex <estevam.neto07@gmail.com>
2022-05-16 00:41:49 -0300

    nada demais

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -2,13 +2,13 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
+    var res = document.getElementById('res')    
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
+        var gênero = ''
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var gênero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
