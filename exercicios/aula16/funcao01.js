4db1a7d307a609e564afe52a467f432d2a961a8a
TrevoYex <estevam.neto07@gmail.com>
2022-05-19 03:12:13 -0300

    aulas função adicionadas

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return  'Impar!'
+    }
+}
+
+let res = parimpar(223)
+console.log(res)
+
+//OU
+
+//console.log(parimpar(4))
