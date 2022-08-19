3d8f953dd5f09f65744c9fc26fd523acb86d64cc
TrevoYex <estevam.neto07@gmail.com>
2022-08-19 15:26:23 -0300

    variaveis e tipos comjavascript

--- DIO/variaveis-e-tipos-com-javascript/index.js
+++ DIO/variaveis-e-tipos-com-javascript/index.js
@@ -1,4 +1,10 @@
-const FIRST_NAME = 'Stephany';
-
-console.log(FIRST_NAME);
-
+function factorial(n) {
+    var product = 1;
+    while(n > 1) {
+        product *= n
+        n--
+    }
+    return product
+    
+}
+console.log(factorial(9))
