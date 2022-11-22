29ffd9dfd26ca416cebb09a6354aee1768606d02
TrevoYex <estevam.neto07@gmail.com>
2022-11-22 17:43:48 -0300

    teste de setInterval e setTimeout

---
+++ teste/script.js
@@ -0,0 +1,46 @@
+// function espera(texto) {
+//     console.log(texto);
+// }
+
+// setTimeout(espera, 1000, 'Depois de 1s');
+
+// setTimeout(() => {
+//     console.log('Ápos 0s?');
+// }, 2000);
+
+// for(let i = 0; i < 20; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 1000 * i);
+// }
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', handleClick);
+
+// function handleClick(event) {
+//     setTimeout(() => {
+//         this.classList.add('active');
+//     }, 1000);
+// }
+
+// function loop(texto) {
+//     console.log(texto);
+// }
+
+// setInterval(loop, 1000, 'Passou 1s');
+
+// let i = 0;
+// setInterval(() => {
+//     console.log(i++)
+// }, 1000);
+
+// const contarAte10 = setInterval(callback, 1000);
+
+// let i = 0;
+// function callback() {
+//     console.log(i++);
+//     if (i > 10) {
+//         clearInterval(contarAte10);
+//     }
+// }
