dc7a6ea3afa34c0a108072356f1a806b00681af0
TrevoYex <estevam.neto07@gmail.com>
2022-11-22 23:09:24 -0300

    dropdown menu

---
+++ dropdown-menu/dropdown-menu.js
@@ -0,0 +1,12 @@
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach((userEvent) => {
+        menu.addEventListener(userEvent, handleClick);
+    });
+});
+
+function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle('active');
+}
