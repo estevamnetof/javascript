b6734a4ca8f40152ea06af14b849d61c2a83bcfc
TrevoYex <estevam.neto07@gmail.com>
2022-12-13 15:02:51 -0300

    To do list

--- TO-DO-2/scripts.js
+++ TO-DO-2/scripts.js
@@ -1,21 +1,26 @@
-function adicionarTarefa() {
+const btn = document.querySelector('#btn');
+
+function adicionarTarefas() {
     const itemLista = document.querySelector('#todoList');
+    const task = document.querySelector('#task');
 
-        const tarefa = document.createElement('li');
-        tarefa.innerHTML = document.querySelector('#task').value;
-        tarefa.classList.add('new-todo-list');
-        itemLista.appendChild(tarefa);
+    const tarefa = document.createElement('label');
+    tarefa.classList.add('new-todo-list');
+    tarefa.innerHTML = task.value
+    itemLista.appendChild(tarefa);
 
-        const remover = document.createElement('button');
-        remover.innerHTML = 'Remover';
-        remover.classList.add('new-todo-list');
-        remover.onclick = () => {
-            itemLista.removeChild(tarefa);
-            itemLista.removeChild(remover);
-        }
-        itemLista.appendChild(remover);
 
-        document.getElementById('#task').value = '';
-
-       
+    const remover = document.createElement('button');
+    remover.innerHTML = '<span class="material-symbols-outlined">delete</span>';
+    remover.classList.add('new-todo-list-btn');
+    remover.onclick = () => {
+        itemLista.removeChild(tarefa);
+        itemLista.removeChild(remover);
+    }
+    itemLista.appendChild(remover);
+    
+    task.value = '';
 }
+
+
+btn.addEventListener('click', adicionarTarefas);
