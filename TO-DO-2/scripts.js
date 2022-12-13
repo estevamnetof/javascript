ef1df26fcd7a8b76a598cd6aecd33934b7ac183e
TrevoYex <estevam.neto07@gmail.com>
2022-12-13 11:56:33 -0300

    mudanças no script

--- TO-DO-2/scripts.js
+++ TO-DO-2/scripts.js
@@ -1,68 +1,21 @@
-'use strict';
+function adicionarTarefa() {
+    const itemLista = document.querySelector('#todoList');
 
-let banco = [
-    {'tarefa': 'Estudar JS', 'status': ''},
-    {'tarefa': 'Netflix', 'status': 'checked'}
-]
+        const tarefa = document.createElement('li');
+        tarefa.innerHTML = document.querySelector('#task').value;
+        tarefa.classList.add('new-todo-list');
+        itemLista.appendChild(tarefa);
 
-const criarItem = (tarefa, status, indice) => {
-    const item = document.createElement('label');
-    item.classList.add('new-todo-list');
-    item.innerHTML = `
-        <input type="checkbox" ${status} data-indice=${indice}>
-        <div>${tarefa}</div>
-        <button id="delete" class="material-symbols-outlined" data-indice=${indice}>delete</button>
-    `
+        const remover = document.createElement('button');
+        remover.innerHTML = 'Remover';
+        remover.classList.add('new-todo-list');
+        remover.onclick = () => {
+            itemLista.removeChild(tarefa);
+            itemLista.removeChild(remover);
+        }
+        itemLista.appendChild(remover);
 
-    document.getElementById('todoList').appendChild(item);
+        document.getElementById('#task').value = '';
+
+       
 }
-
-const limparTarefas = () => {
-    const todoList = document.querySelector('#todoList');
-    while(todoList.firstChild) {
-        todoList.removeChild(todoList.lastChild);
-    }
-}
-
-const atualizarTela = () => {
-    limparTarefas();
-    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
-}
-
-const inserirItem = (event) => {
-    const tecla = event.key;
-    const botao = event.click;
-    const texto = event.target.value;
-    if(tecla === 'Enter') {
-        banco.push({'tarefa': texto, 'status': ''});
-        atualizarTela();
-        event.target.value = '';
-    }
-}
-
-const removerItem = (indice) => {
-    banco.splice(indice, 1);
-    atualizarTela();
-}
-
-const atualizarItem = (indice) => {
-    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
-    atualizarTela();
-}
-
-const clickItem = (event) => {
-    const elemento = event.target;
-    if(elemento.type === 'button') {
-        const indice = elemento.dataset.indice;
-        removerItem(indice);
-    } else if(elemento.type === 'checkbox') {
-        const indice = elemento.dataset.indice;
-        atualizarItem(indice);
-    }
-}
-
-document.getElementById('task').addEventListener('keypress', inserirItem);
-document.getElementById('todoList').addEventListener('click', clickItem);
-
-
-atualizarTela();
