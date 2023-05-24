2a6360d34b1f0630f1bafd31599e6a58f458e52c
estevamnetof <estevam.neto07@gmail.com>
2023-05-24 16:07:59 -0300

    login - teste

--- projetos/login/script.js
+++ projetos/login/script.js
@@ -1,9 +1,38 @@
 const eyes = document.querySelector('#verSenha');
 let senha = document.querySelector('#senha');
+let labelSenha = document.querySelector('#labelSenha');
 
 const eyesConfirm = document.querySelector('#verConfirmSenha');
 let senhaConfirm = document.querySelector('#confirmSenha');
+let labelSenhaConfirm = document.querySelector('#labelConfirmSenha');
 
+// Verificação de caracteres
+senha.addEventListener('keyup', () => {
+    if (senha.value.length <= 8) {
+        labelSenha.innerHTML = 'Senha * Sua senha deve conter pelo menos 8 caracteres'
+        labelSenha.style.color = 'red'
+        senha.style.borderBottom = '2px solid red'
+    } else {
+        labelSenha.innerHTML = 'Senha'
+        labelSenha.style.color = 'green'
+        senha.style.borderBottom = '2px solid green'
+    }
+});
+
+// Senhas conferem
+senhaConfirm.addEventListener('keyup', () => {
+    if (senha.value != senhaConfirm.value) {
+        labelSenhaConfirm.innerHTML = 'As senhas não conferem'
+        labelSenhaConfirm.style.color = 'red'
+        senhaConfirm.style.borderBottom = '2px solid red'
+    } else {
+        labelSenhaConfirm.innerHTML = 'Confirmar Senha'
+        labelSenhaConfirm.style.color = 'green'
+        senhaConfirm.style.borderBottom = '2px solid green'
+    }
+});
+
+// Visualização da senha
 eyes.addEventListener('click', () => {
     if (senha.type === 'password') {
         senha.setAttribute('type', 'text');
@@ -18,4 +47,4 @@ eyesConfirm.addEventListener('click', () => {
     } else {
         senhaConfirm.setAttribute('type', 'password');
     }
-})
+});
