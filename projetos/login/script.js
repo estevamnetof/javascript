d1248c28d475a21246400c86144bebb7a136d88a
estevamnetof <estevam.neto07@gmail.com>
2023-05-31 00:05:36 -0300

    refatorando

--- projetos/login/script.js
+++ projetos/login/script.js
@@ -6,6 +6,24 @@ const eyesConfirm = document.querySelector('#verConfirmSenha');
 let senhaConfirm = document.querySelector('#confirmSenha');
 let labelSenhaConfirm = document.querySelector('#labelConfirmSenha');
 
+const msgError = document.querySelector('#msgError');
+const msgSucess = document.querySelector('#msgSucess');
+
+// Funções
+function viewPassword(password) {
+    if (password.type === 'password') {
+        password.setAttribute('type', 'text')
+    } else {
+        password.setAttribute('type', 'password');
+    }
+}
+
+function inputCorrect(item, label, campo) {
+    label.innerHTML = campo
+    label.style.color = 'green'
+    item.style.borderBottom = '2px solid green'
+}
+
 // Verificação de caracteres
 senha.addEventListener('keyup', () => {
     if (senha.value.length <= 8) {
@@ -13,9 +31,7 @@ senha.addEventListener('keyup', () => {
         labelSenha.style.color = 'red'
         senha.style.borderBottom = '2px solid red'
     } else {
-        labelSenha.innerHTML = 'Senha'
-        labelSenha.style.color = 'green'
-        senha.style.borderBottom = '2px solid green'
+        inputCorrect(senha, labelSenha, 'Senha');
     }
 });
 
@@ -26,25 +42,15 @@ senhaConfirm.addEventListener('keyup', () => {
         labelSenhaConfirm.style.color = 'red'
         senhaConfirm.style.borderBottom = '2px solid red'
     } else {
-        labelSenhaConfirm.innerHTML = 'Confirmar Senha'
-        labelSenhaConfirm.style.color = 'green'
-        senhaConfirm.style.borderBottom = '2px solid green'
+        inputCorrect(senhaConfirm, labelSenhaConfirm, 'Confirmar Senha');
     }
 });
 
 // Visualização da senha
 eyes.addEventListener('click', () => {
-    if (senha.type === 'password') {
-        senha.setAttribute('type', 'text');
-    } else {
-        senha.setAttribute('type', 'password');
-    }
+    viewPassword(senha);
 });
 
 eyesConfirm.addEventListener('click', () => {
-    if (senhaConfirm.type === 'password') {
-        senhaConfirm.setAttribute('type', 'text');
-    } else {
-        senhaConfirm.setAttribute('type', 'password');
-    }
+    viewPassword(senhaConfirm);
 });
