8033c0cd52ead3f7936e1eec0c239e5af8e08a8d
estevamnetof <estevam.neto07@gmail.com>
2023-06-29 03:01:32 -0300

    teste de factory function

--- teste/script.js
+++ teste/script.js
@@ -1,27 +1,35 @@
-let objetosGuardados = [];
+// let objetosGuardados = [];
 
-function guardarObjeto(novoObjeto) {
-    objetosGuardados.push(novoObjeto)
-}
+// function guardarObjeto(novoObjeto) {
+//     objetosGuardados.push(novoObjeto)
+// }
 
-function apresentarObjetos() {
-    if(objetosGuardados.length > 0) {
-        console.log('Os objetos guardados são: ');
-        objetosGuardados.forEach((objeto, indice) => {
-            console.log('Objeto ' + (indice + 1) + ':');
-            console.log(objeto);
-        });
-    } else {
-        console.log('Nenhum objeto foi guardado ainda');
-    }
-}
+// function apresentarObjetos() {
+//     if(objetosGuardados.length > 0) {
+//         console.log('Os objetos guardados são: ');
+//         objetosGuardados.forEach((objeto, indice) => {
+//             console.log('Objeto ' + (indice + 1) + ':');
+//             console.log(objeto);
+//         });
+//     } else {
+//         console.log('Nenhum objeto foi guardado ainda');
+//     }
+// }
 
-let objeto1 = { nome: 'Objeto 1', valor: 42};
-let objeto2 = { nome: 'Objeto 2', valor: 100};
-let objeto3 = { nome: 'Objeto 3', valor: 7};
+// let objeto1 = { nome: 'Objeto 1', valor: 42};
+// let objeto2 = { nome: 'Objeto 2', valor: 100};
+// let objeto3 = { nome: 'Objeto 3', valor: 7};
 
-guardarObjeto(objeto1);
-guardarObjeto(objeto2);
-guardarObjeto(objeto3);
+// guardarObjeto(objeto1);
+// guardarObjeto(objeto2);
+// guardarObjeto(objeto3);
 
-apresentarObjetos();
+// apresentarObjetos();
+
+
+function saveValue(name, value) {
+    localStorage[name] = value;
+  }
+  
+  saveValue('Estevam', 25);
+  saveValue('Neto', 25);
