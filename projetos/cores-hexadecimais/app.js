d62ca2aebead15b34a52e5e900fb57bb04dbbdfb
estevamnetof <estevam.neto07@gmail.com>
2023-11-14 02:46:23 -0300

    cores hexadecimais

--- projetos/cores-hexadecimais/app.js
+++ projetos/cores-hexadecimais/app.js
@@ -3,7 +3,10 @@ const body = document.querySelector('body');
 const hexaValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const valor = document.querySelector('.valor-hexa');
 
+const copia = document.querySelector('.copia');
+
 btn.addEventListener('click', changeHex);
+copia.addEventListener('click', copyColor);
 
 function changeHex() {
     let hex = '#';
@@ -15,4 +18,14 @@ function changeHex() {
 
     valor.textContent = hex;
     body.style.background = hex;
+
+    copia.style.display = 'block';
+}
+
+function copyColor() {
+    navigator.clipboard.writeText(valor.textContent);
+
+    setTimeout(() => {
+        copia.style.display = 'none';
+    }, 1000);
 }
